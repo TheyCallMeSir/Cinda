4dbd62152da09b402f40feb2d556b3e0c7747874
HasanAouzoun <hasanozon4@gmail.com>
2019-10-26 23:57:11 +0100

    connected to database

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+// connecting to Database
+const mongoose = require('mongoose')
+const db = "mongodb+srv://admin:zFGRKXn4aHKfKdX@myproject-k5h8e.mongodb.net/Cinda?retryWrites=true"
+
+mongoose.connect(db, err => {
+    if (err) {
+      console.error('Error!' + err)
+    } else {
+      console.log('Connected to Database')
+    }
+  });
+
 // import API Routers
 const main = require('./roters/main')
 
